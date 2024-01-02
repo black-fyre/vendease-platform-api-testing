55a64949040b24b4abaf08e6754be46f106b6d5a
Dahunsi Fehintoluwa <dahunsi.fehinti@gmail.com>
2024-01-02 23:12:13 +0100

    Add assertion text

--- test/create_user.js
+++ test/create_user.js
@@ -9,23 +9,20 @@ const { performance } = require('perf_hooks');
 
 describe('ResReq API Tests', () => {
     users.forEach((user) => {
-        it('should create a user', () => {
+        it('should create a user successfully', () => {
             //Record the start time before making the API request
             const startTime = performance.now();
             return request
                 .post('users')
                 .send(user)
                 .then((res) => {
-                    // Assert the response time is within 2000 milliseconds
-                    expect(performance.now() - startTime).to.be.lessThan(2000)
-                    expect(res.statusCode).to.equal(201);
-                    expect(validate(res.body), "JSON schema is valid").to.be.true;
-                    expect(res.header).to.have.property('content-type');
-                    expect(res.header["content-type"]).to.include('application/json');
-
-
-                    //console.log(JSON.stringify(res, null, "\t"))
-
+                    expect(performance.now() - startTime, "Response time should be less than 2s").to.be.lessThan(2000)
+                    expect(res.statusCode, "Status Code should be 201").to.equal(201);
+                    expect(validate(res.body), "Response schema should match JSON schema")
+                        .to.be.true;
+                    expect(res.header, "Headers should contain Content-Type").to.have.property('content-type');
+                    expect(res.header["content-type"], "Content-Type should be application/json")
+                        .to.include('application/json');
                 })
         })
     })
